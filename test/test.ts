2bed2791e5d775b088d6d088a602d6b530d90f2b
慕陶 <jihui.xjh@alibaba-inc.com>
2016-03-24 21:02:39 +0800

    update

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,13 @@
-import * as test from 'blue-tape'
 import isCallable = require('is-callable');
+import urllib = require('urllib');
+import test = require('blue-tape');
+import onchange = require('onchange');
+
+test('exists', (t)=>{
+  t.true(isCallable(urllib.create), 'create is callable');
+  t.true(isCallable(urllib.request), 'request is callable');
+  t.true(isCallable(urllib.request), 'request is callable');
+  t.end();
+})
 
 
